bcc382efa14857ba7e83e1b1e49595f9b28f63ec
radzek15 <radzek15@wp.pl>
2024-03-21 22:16:09 -0400

    frontend

--- core_apps/questions/urls.py
+++ core_apps/questions/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import QuestionListCreateView, QuestionRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('question/', QuestionListCreateView.as_view(), name='docker-lc'),
-    path('question/<int:id>/', QuestionRetrieveUpdateDestroyView.as_view(), name='docker-rud'),
+    path('', QuestionListCreateView.as_view(), name='docker-lc'),
+    path('<int:id>/', QuestionRetrieveUpdateDestroyView.as_view(), name='docker-rud'),
 ]
