c117270f54f6867b95094af3219a892715194524
radzek15 <radzek15@wp.pl>
2024-03-27 11:27:19 -0400

    vue

--- client/src/components/Quiz/DemoPlay.vue
+++ client/src/components/Quiz/DemoPlay.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <div class="d-flex justify-content-center align-items-center" style="height: calc(100vh);">
     <router-link to="/demo-quiz" class="btn btn-primary custom-btn">DEMO QUIZ</router-link>
-    <router-link to="/create-quiz" class="btn btn-primary custom-btn">DEMO QUIZ</router-link>
+    <router-link to="/create-quiz" class="btn btn-primary custom-btn">CREATE QUIZ</router-link>
   </div>
 </template>
 
--- client/src/components/Quiz/DemoQuiz.vue
+++ client/src/components/Quiz/DemoQuiz.vue
@@ -1,3 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Demo Quiz</h1>
+    <div v-for="q in questions" :key="q.id">
+      <p>{{q.id}}. {{q.question_description}}</p>
+      <div v-for="(choice, index) in getChoices(q)" :key="index">
+        <input
+          type="radio"
+          :name="'question_' + q.id"
+          :value="choice"
+          v-model="q.selectedChoice"
+        >
+        <label>{{ choice }}</label>
+      </div>
+    </div>
+    <button @click="submitQuiz">Submit Quiz</button>
+  </div>
+</template>
+
 <script>
 import axios from "axios";
 
@@ -5,37 +24,38 @@ export default {
   name: "DemoQuiz",
   data() {
     return {
-      questions: []
+      questions: [],
+      showResult: false,
+      correctAnswers: 0
     };
   },
   created() {
     axios.get('http://localhost:8080/api/questions/')
       .then(response => {
         if (response.data.results && response.data.results.length > 0) {
-          this.questions = response.data.results;
+          this.questions = response.data.results.map(question => ({
+            ...question,
+            selectedChoice: null
+          }));
         }
       })
       .catch(error => {
         console.error('Error fetching questions:', error);
       });
+  },
+  methods: {
+    getChoices(question) {
+      return [question.correct_answer, question.wrong_answer1, question.wrong_answer2, question.wrong_answer3];
+    },
+    submitQuiz() {
+      this.correctAnswers = this.questions.reduce((count, question) => {
+        return question.selectedChoice === question.correct_answer ? count + 1 : count;
+      }, 0);
+      alert(`Your score: ${this.correctAnswers}`);
+    },
   }
-}
+};
 </script>
-
-<template>
-  <div>
-    <h1>Demo Quiz</h1>
-    <div v-for="q in questions" :key="q.id">
-      <p> {{q.id}}. {{q.question_description}} </p>
-      <label>
-        <input type="radio"> {{q.correct_answer}} <br>
-        <input type="radio"> {{q.wrong_answer1}} <br>
-        <input type="radio"> {{q.wrong_answer2}} <br>
-        <input type="radio"> {{q.wrong_answer3}}
-      </label>
-    </div>
-  </div>
-</template>
 
 <style scoped>
 
