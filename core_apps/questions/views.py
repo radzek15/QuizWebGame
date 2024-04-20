fbb4fdf3b3271178566c2477abb8d4fb3a828e8f
radzek15 <radzek15@wp.pl>
2024-04-20 11:09:06 -0400

    permission adjusted

--- core_apps/questions/views.py
+++ core_apps/questions/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Question
 from .pagination import QuestionPagination
@@ -9,9 +10,11 @@ class QuestionListCreateView(generics.ListCreateAPIView):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
     pagination_class = QuestionPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class QuestionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
     lookup_field = 'id'
+    permission_classes = [IsAuthenticatedOrReadOnly]
--- core_apps/quizzes/views.py
+++ core_apps/quizzes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Quiz
 from .serializers import QuizSerializer
@@ -7,9 +8,11 @@ from .serializers import QuizSerializer
 class QuizListCreateView(generics.ListCreateAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class QuizRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
     lookup_field = 'id'
+    permission_classes = [IsAuthenticatedOrReadOnly]
